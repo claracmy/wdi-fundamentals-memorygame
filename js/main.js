d6c0e82626ad7a9ee09ea7940f836d48acb0a42b
claracmy <cheung.cmy@gmail.com>
2017-08-29 14:42:35 +0100

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,17 +1,21 @@
 cards = ["queen","queen","king","king"];
 cardsInPlay = [];
-var cardOne = cards[0];
-var cardTwo = cards[2];
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
 
-if (cardsInPlay.length === 2) {
-  if (cardsInPlay[0] === cardsInPlay[1]){
-    alert("You found a match!");
-  } else {
-    alert("Sorry, try asgain.");
-  }
-}
+var checkForMatch = function(){
+ if (cardsInPlay.length === 2){
+    if (cardsInPlay[0] === cardsInPlay[1]){
+    console.log("You found a match!");
+    } else {
+    console.log("Sorry, try again.");
+    };
+  };
+};
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
+var flipCard = function(cardId){
+  console.log("User flipped " + cards[cardId] + ".");
+  cardsInPlay.push(cards[cardId]);
+  checkForMatch();
+};
+
+flipCard(0);
+flipCard(2);
