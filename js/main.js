319821d553017d668242561cb573bdc5b850f3f4
claracmy <cheung.cmy@gmail.com>
2017-09-08 16:33:50 +0100

    created gamme logic

--- js/main.js
+++ js/main.js
@@ -26,20 +26,31 @@ cardsInPlay = [];
 var checkForMatch = function(){
  if (cardsInPlay.length === 2){
     if (cardsInPlay[0] === cardsInPlay[1]){
-    console.log("You found a match!");
+    alert("You found a match!");
     } else {
-    console.log("Sorry, try again.");
+    alert("Sorry, try again.");
     };
   };
 };
 
-var flipCard = function(cardId){
+var flipCard = function(){
+  var cardId = this.getAttribute('data-id');
   console.log("User flipped " + cards[cardId].rank + ".");
   console.log(cards[cardId].cardImage);
   console.log(cards[cardId].suit);
   cardsInPlay.push(cards[cardId].rank);
+  this.setAttribute('src',cards[cardId].cardImage);
   checkForMatch();
 };
 
-flipCard(0);
-flipCard(2);
+var createBoard = function(){
+  for (var i = 0; i < cards.length; i++) {
+    var cardElement = document.createElement('img');
+    cardElement.setAttribute('src', "images/back.png");
+    cardElement.setAttribute('data-id', i);
+    cardElement.addEventListener('click', flipCard);
+    document.getElementById('game-board').appendChild(cardElement);
+  };
+};
+
+createBoard();
